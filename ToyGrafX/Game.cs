e983fd44380a67950843f047e433e812deeaac8a
John Kerr <john@jmkerr.com>
2019-06-27 00:37:53 +0100

    Add project files.

---
+++ ToyGrafX/Game.cs
@@ -0,0 +1,73 @@
+﻿namespace ToyGrafX
+{
+    using System;
+    using OpenTK;
+    using OpenTK.Graphics;
+    using OpenTK.Graphics.ES20;
+    using OpenTK.Input;
+
+    public class Game : GameWindow
+    {
+        public Game(int width, int height, string title) :
+            base(width, height, GraphicsMode.Default, title)
+        { }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            //Code goes here
+            VertexBufferObject = GL.GenBuffer();
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            shader = new Shader("shader.vert", "shader.frag");
+
+            base.OnLoad(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            //Code goes here.
+
+            Context.SwapBuffers();
+            base.OnRenderFrame(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            base.OnResize(e);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(VertexBufferObject);
+
+            shader.Dispose();
+
+            base.OnUnload(e);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            var input = Keyboard.GetState();
+            if (input.IsKeyDown(Key.Escape))
+                Exit();
+            base.OnUpdateFrame(e);
+        }
+
+        int VertexBufferObject;
+        float[] vertices = {
+            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+            0.5f, -0.5f, 0.0f,  //Bottom-right vertex
+            0.0f,  0.5f, 0.0f   //Top vertex
+        };
+
+        Shader shader;
+    }
+}
---
+++ ToyGrafX/Shader.cs
@@ -0,0 +1,91 @@
+﻿namespace ToyGrafX
+{
+    using OpenTK.Graphics.ES20;
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public class Shader
+    {
+        int Handle;
+
+        public Shader(string vertexPath, string fragmentPath)
+        {
+            int VertexShader, FragmentShader;
+
+            string VertexShaderSource;
+
+            using (StreamReader reader = new StreamReader(vertexPath, Encoding.UTF8))
+            {
+                VertexShaderSource = reader.ReadToEnd();
+            }
+
+            string FragmentShaderSource;
+
+            using (StreamReader reader = new StreamReader(fragmentPath, Encoding.UTF8))
+            {
+                FragmentShaderSource = reader.ReadToEnd();
+            }
+
+            VertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(VertexShader, VertexShaderSource);
+
+            FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(FragmentShader, FragmentShaderSource);
+
+            GL.CompileShader(VertexShader);
+
+            string infoLogVert = GL.GetShaderInfoLog(VertexShader);
+            if (infoLogVert != string.Empty)
+                Console.WriteLine(infoLogVert);
+
+            GL.CompileShader(FragmentShader);
+
+            string infoLogFrag = GL.GetShaderInfoLog(FragmentShader);
+
+            if (infoLogFrag != string.Empty)
+                Console.WriteLine(infoLogFrag);
+
+            Handle = GL.CreateProgram();
+
+            GL.AttachShader(Handle, VertexShader);
+            GL.AttachShader(Handle, FragmentShader);
+
+            GL.LinkProgram(Handle);
+
+            GL.DetachShader(Handle, VertexShader);
+            GL.DetachShader(Handle, FragmentShader);
+            GL.DeleteShader(FragmentShader);
+            GL.DeleteShader(VertexShader);
+        }
+
+        void Use()
+        {
+            GL.UseProgram(Handle);
+        }
+
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                GL.DeleteProgram(Handle);
+
+                disposedValue = true;
+            }
+        }
+
+        ~Shader()
+        {
+            GL.DeleteProgram(Handle);
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
