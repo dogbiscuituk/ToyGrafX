28cb5ab6131376107484ad8a6318f6e266fdb37f
John Kerr <john@jmkerr.com>
2019-06-27 00:46:42 +0100

    Update Game.cs

--- ToyGrafX/Game.cs
+++ ToyGrafX/Game.cs
@@ -62,7 +62,8 @@
         }
 
         int VertexBufferObject;
-        float[] vertices = {
+        float[] vertices =
+        {
             -0.5f, -0.5f, 0.0f, //Bottom-left vertex
             0.5f, -0.5f, 0.0f,  //Bottom-right vertex
             0.0f,  0.5f, 0.0f   //Top vertex
