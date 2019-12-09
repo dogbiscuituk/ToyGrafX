c0c9c16595807aae62d1203ce94fd27a5276d331
John Kerr <john@jmkerr.com>
2019-12-09 18:30:25 +0000

    New editor types

---
+++ ToyGraf.Controls/GLSLShaderEditor.cs
@@ -0,0 +1,23 @@
+﻿namespace ToyGraf.Controls
+{
+    using System;
+    using System.Windows.Forms;
+    using FastColoredTextBoxNS;
+
+    public partial class GLSLShaderEditor : UserControl
+    {
+        public GLSLShaderEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, FastColoredTextBoxNS.TextChangedEventArgs e)
+        {
+            var textBox = (FastColoredTextBox)sender;
+            var h = Height - textBox.Height;
+            textBox.Parent.Height = 14 * Math.Max(textBox.LinesCount, 1);
+            h += textBox.Height;
+            Height = h;
+        }
+    }
+}
